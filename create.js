af94e3d44230bed67b756445162dc615af8285b9
Mariusz Nowak <mariusz@medikoo.com>
2013-10-23 17:28:47 +0200

    Memoize slices, maps and filters

--- create.js
+++ create.js
@@ -5,21 +5,27 @@ var isSubclassable = require('es5-ext/array/_is-subclassable')
   , isCopy         = require('es5-ext/array/#/is-copy')
   , validFunction  = require('es5-ext/function/valid-function')
   , toInt          = require('es5-ext/number/to-int')
+  , assign         = require('es5-ext/object/assign')
   , eq             = require('es5-ext/object/eq')
   , mixin          = require('es5-ext/object/mixin-prototypes')
   , setPrototypeOf = require('es5-ext/object/set-prototype-of')
   , callable       = require('es5-ext/object/valid-callable')
   , value          = require('es5-ext/object/valid-value')
   , d              = require('d/d')
+  , lazy           = require('d/lazy')
   , ee             = require('event-emitter')
   , memoize        = require('memoizee/lib/regular')
   , createReadOnly = require('./create-read-only')
 
   , bind = Function.prototype.bind, max = Math.max
-  , defineProperty = Object.defineProperty
+  , defineProperties = Object.defineProperties
   , getPrototypeOf = Object.getPrototypeOf
   , hasOwnProperty = Object.prototype.hasOwnProperty
   , concat, arrSplice;
+
+require('memoizee/lib/ext/ref-counter');
+require('memoizee/lib/ext/resolvers');
+require('memoizee/lib/ext/dispose');
 
 if (!isSubclassable) {
 	concat = require('es5-ext/array/#/concat');
@@ -28,7 +34,7 @@ if (!isSubclassable) {
 
 module.exports = memoize(function (Constructor) {
 	var Observable, forEach, pop, push, reverse, shift, sort, splice, unshift
-	  , ReadOnly;
+	  , ReadOnly, slice, filter, map;
 
 	validFunction(Constructor);
 	Observable = function (len) {
@@ -51,7 +57,154 @@ module.exports = memoize(function (Constructor) {
 			arrSplice : Constructor.prototype.splice;
 	unshift = Constructor.prototype.unshift;
 
-	Observable.prototype = ee(Object.create(Array.prototype, {
+	slice = function (start, end) {
+		var result, refresh, listener, disposed;
+		result = new ReadOnly();
+		refresh = function () {
+			var s = start, e = end, length = this.length, changed, i;
+			if (s < 0) s = max(length + s, 0);
+			else if (s > length) s = length;
+			if (e < 0) e = max(length + e, 0);
+			else if (e > length) e = length;
+			if (s > e) s = e;
+			if ((e - s) !== result.length) changed = true;
+			result.length = e - s;
+			i = 0;
+			while (s !== e) {
+				if (hasOwnProperty.call(this, s)) {
+					if (!hasOwnProperty.call(result, i) || !eq(result[i], this[s])) {
+						changed = true;
+						result[i] = this[s];
+					}
+				} else if (hasOwnProperty.call(result, i)) {
+					changed = true;
+					delete result[i];
+				}
+				++i;
+				++s;
+			}
+			return changed;
+		}.bind(this);
+		refresh();
+		this.on('change', listener = function () {
+			if (refresh()) result.emit('change');
+		});
+		defineProperties(result, {
+			unref: d(function () {
+				if (disposed) return;
+				this.slice.clearRef(start, end);
+			}.bind(this)),
+			_onDispose: d(function () {
+				this.off('change', listener);
+				disposed = true;
+			}.bind(this))
+		});
+		return result;
+	};
+
+	filter = function (callbackFn/*, thisArg*/) {
+		var result, listener, refresh, thisArg, cb, disposed;
+		(value(this) && callable(callbackFn));
+		thisArg = arguments[1];
+		cb = memoize(bind.call(callbackFn, thisArg), { length: 1 });
+		result = new ReadOnly();
+		refresh = function () {
+			var i = 0, changed;
+			forEach.call(this, function (val, j, self) {
+				if (cb(val, j, self)) {
+					if (!hasOwnProperty.call(result, i) || !eq(result[i], val)) {
+						changed = true;
+						result[i] = val;
+					}
+					++i;
+				}
+			});
+			if (result.length !== i) {
+				changed = true;
+				result.length = i;
+			}
+			return changed;
+		}.bind(this);
+		refresh();
+		this.on('change', listener = function () {
+			if (refresh()) result.emit('change');
+		});
+		defineProperties(result, {
+			refresh: d(function (index) {
+				var filtered;
+				index = index >>> 0;
+				if (!this.hasOwnProperty(index)) return;
+				filtered = Boolean(cb(this[index]));
+				cb.clear(this[index]);
+				if (Boolean(cb(this[index])) !== filtered) listener();
+			}.bind(this)),
+			refreshAll: d(function () {
+				cb.clearAll();
+				listener();
+			}),
+			unref: d(function () {
+				if (disposed) return;
+				this.filter.clearRef(callbackFn, thisArg);
+			}.bind(this)),
+			_onDispose: d(function () {
+				this.off('change', listener);
+				disposed = true;
+			}.bind(this))
+		});
+		return result;
+	};
+
+	map = function (callbackFn/*, thisArg*/) {
+		var result, listener, refresh, thisArg, disposed, cb;
+		(value(this) && callable(callbackFn));
+		thisArg = arguments[1];
+		cb = memoize(bind.call(callbackFn, thisArg), { length: 1 });
+		result = new ReadOnly();
+		refresh = function () {
+			var changed;
+			if (result.length !== this.length) {
+				changed = true;
+				result.length = this.length;
+			}
+			forEach.call(this, function (val, i, self) {
+				val = cb(val, i, self);
+				if (!hasOwnProperty.call(result, i) || !eq(result[i], val)) {
+					changed = true;
+					result[i] = val;
+				}
+			});
+			return changed;
+		}.bind(this);
+		refresh();
+		this.on('change', listener = function () {
+			if (refresh()) result.emit('change');
+		});
+		defineProperties(result, {
+			refresh: d(function (index) {
+				var val;
+				index = index >>> 0;
+				if (!this.hasOwnProperty(index)) return;
+				val = cb(this[index]);
+				cb.clear(this[index]);
+				if (!eq(cb(this[index]), val)) listener();
+			}.bind(this)),
+			refreshAll: d(function () {
+				cb.clearAll();
+				listener();
+			}),
+			unref: d(function () {
+				if (disposed) return;
+				this.map.clearRef(callbackFn, thisArg);
+			}.bind(this)),
+			_onDispose: d(function () {
+				this.off('change', listener);
+				disposed = true;
+			}.bind(this))
+		});
+		return result;
+	};
+
+	Observable.prototype = ee(Object.create(Array.prototype, assign({
 		constructor: d(Observable),
 		pop: d(function () {
 			var element;
@@ -109,126 +262,38 @@ module.exports = memoize(function (Constructor) {
 			result = unshift.apply(this, arguments);
 			this.emit('change');
 			return result;
-		}),
-
-		slice: d(function (start, end) {
-			var result, refresh, listener;
-			start = toInt(start);
-			end = (end === undefined) ? Infinity : toInt(end);
-			result = new ReadOnly();
-			refresh = function () {
-				var s = start, e = end, length = this.length, changed, i;
-				if (s < 0) s = max(length + s, 0);
-				else if (s > length) s = length;
-				if (e < 0) e = max(length + e, 0);
-				else if (e > length) e = length;
-				if (s > e) s = e;
-				if ((e - s) !== result.length) changed = true;
-				result.length = e - s;
-				i = 0;
-				while (s !== e) {
-					if (hasOwnProperty.call(this, s)) {
-						if (!hasOwnProperty.call(result, i) || !eq(result[i], this[s])) {
-							changed = true;
-							result[i] = this[s];
-						}
-					} else if (hasOwnProperty.call(result, i)) {
-						changed = true;
-						delete result[i];
-					}
-					++i;
-					++s;
-				}
-				return changed;
-			}.bind(this);
-			refresh();
-			this.on('change', listener = function () {
-				if (refresh()) result.emit('change');
-			});
-			result.once('unlink', this.off.bind(this, 'change', listener));
-			return result;
-		}),
-		filter: d(function (callbackFn/*, thisArg*/) {
-			var result, listener, refresh;
-			(value(this) && callable(callbackFn));
-			callbackFn = memoize(bind.call(callbackFn, arguments[1]), { length: 1 });
-			result = new ReadOnly();
-			refresh = function () {
-				var i = 0, changed;
-				forEach.call(this, function (val, j, self) {
-					if (callbackFn(val, j, self)) {
-						if (!hasOwnProperty.call(result, i) || !eq(result[i], val)) {
-							changed = true;
-							result[i] = val;
-						}
-						++i;
-					}
-				});
-				if (result.length !== i) {
-					changed = true;
-					result.length = i;
-				}
-				return changed;
-			}.bind(this);
-			refresh();
-			this.on('change', listener = function () {
-				if (refresh()) result.emit('change');
-			});
-			result.once('unlink', this.off.bind(this, 'change', listener));
-			defineProperty(result, 'refresh', d(function (index) {
-				var filtered;
-				index = index >>> 0;
-				if (!this.hasOwnProperty(index)) return;
-				filtered = Boolean(callbackFn(this[index]));
-				callbackFn.clear(this[index]);
-				if (Boolean(callbackFn(this[index])) !== filtered) listener();
-			}.bind(this)));
-			defineProperty(result, 'refreshAll', d(function () {
-				callbackFn.clearAll();
-				listener();
-			}));
-			return result;
-		}),
-		map: d(function (callbackFn/*, thisArg*/) {
-			var result, listener, refresh;
-			(value(this) && callable(callbackFn));
-			callbackFn = memoize(bind.call(callbackFn, arguments[1]), { length: 1 });
-			result = new ReadOnly();
-			refresh = function () {
-				var changed;
-				if (result.length !== this.length) {
-					changed = true;
-					result.length = this.length;
-				}
-				forEach.call(this, function (val, i, self) {
-					val = callbackFn(val, i, self);
-					if (!hasOwnProperty.call(result, i) || !eq(result[i], val)) {
-						changed = true;
-						result[i] = val;
-					}
-				});
-				return changed;
-			}.bind(this);
-			refresh();
-			this.on('change', listener = function () {
-				if (refresh()) result.emit('change');
-			});
-			result.once('unlink', this.off.bind(this, 'change', listener));
-			defineProperty(result, 'refresh', d(function (index) {
-				var val;
-				index = index >>> 0;
-				if (!this.hasOwnProperty(index)) return;
-				val = callbackFn(this[index]);
-				callbackFn.clear(this[index]);
-				if (!eq(callbackFn(this[index]), val)) listener();
-			}.bind(this)));
-			defineProperty(result, 'refreshAll', d(function () {
-				callbackFn.clearAll();
-				listener();
-			}));
-			return result;
 		})
-	}));
+	}, lazy({
+		slice: d(function () {
+			return memoize(slice.bind(this), {
+				resolvers: [toInt, function (val) { return (val === undefined) ?
+						Infinity : toInt(val); }],
+				refCounter: true,
+				dispose: function (arr) {
+					arr._onDispose();
+					delete arr._onDispose;
+				}
+			});
+		}),
+		filter: d(function () {
+			return memoize(filter.bind(this), {
+				refCounter: true,
+				dispose: function (arr) {
+					arr._onDispose();
+					delete arr._onDispose;
+				}
+			});
+		}),
+		map: d(function () {
+			return memoize(map.bind(this), {
+				refCounter: true,
+				dispose: function (arr) {
+					arr._onDispose();
+					delete arr._onDispose;
+				}
+			});
+		})
+	}))));
 
 	if (isSubclassable) {
 		Object.defineProperty(Observable.prototype, 'concat', d(concat));
